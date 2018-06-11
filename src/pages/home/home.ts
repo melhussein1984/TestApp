e820d2018db29d383290b619ecfce2b7cd2ef5dd
melhussein1984 <mohmmedali@hotmail.com>
2018-06-12 01:21:05 +0300

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todosListOfItems = [];
 
+  constructor(public navCtrl: NavController, private AlCtrl: AlertController) {
+
+  }
+
+  OpenToDoAlert() {
+    let addTodoAlert = this.AlCtrl.create({
+      title: "Add a Todo",
+      message: "Please enter the details",
+      inputs: [
+        {
+          type: "text",
+          placeholder: "Add your item name here",
+          name: "addTodoInputText"
+        }
+      ],
+
+      buttons: [
+        {
+          text: "add",
+          handler: (inputData) => {
+            let todoTest = inputData.addTodoInputText;
+            this.todosListOfItems.push(todoTest);
+          }
+        },
+        {
+          text: "cancel"
+        }
+      ]
+    });
+    addTodoAlert.present();
   }
 
 }
